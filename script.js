86b7679b3b87cf8baf548992b3163000042d0c64
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-01-15 00:25:55 -0500

    added second row that will feature Today's Weather in card form

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
-var searchHistory = [];
-
+console.log("hello")
 //wire up search button to pull city from ajax call for general search
 $(".search").on("click", 
 
@@ -9,55 +8,21 @@ function searchCity(data){
 var searchbar = $(".searchInput").val()
 console.log(searchbar)
 
-// mainWeatherPull(searchbar)
 
-searchHistory.push($(".searchInput").val())
-
-$(".searchInput").val("");
-$(".recentSearches").text("");
-
-$.each(searchHistory, function (index, value) {
-    $(".recentSearches").append("<li class='cityName' onclick='addCity("+index+")'>" + value +  '</li>');
-});
-
-function addCity(id) {
-$(".searchInput").val(searchHistory[id]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-}
-)
-
-
-
-
-})
 
 
 // first ajax call with weather variables (temp humidity windspeed uvindex) for today's weather
 // function mainWeatherPull (searchbar){
 
-// console.log(searchbar)
+console.log(searchbar)
 
-// $.ajax({
-// url:"https://openweathermap.org/api" + searchbar + "889e002b8fmsh68a55d24a31d654p1e691ajsn4b5a65ae9eb2",
-// method:"GET"
-// }).then(function(dataPull){
-// console.log(dataPull)
-// })
-// }
+$.ajax({
+url:"https://openweathermap.org/api" + searchbar + "889e002b8fmsh68a55d24a31d654p1e691ajsn4b5a65ae9eb2",
+method:"GET"
+}).then(function(dataPull){
+console.log(dataPull)
+})
+})
 
 
 
@@ -73,18 +38,10 @@ $(".searchInput").val(searchHistory[id]);
 
 
 
+})
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-    
