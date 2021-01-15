8143eb28e618121db2eda18ebe674f9252c5a990
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-01-15 15:09:41 -0500

    API TEST FINALLY WORKSgit add -Agit add -A Also fixed cdn issue causing AJAX call to fail

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 $(document).ready(function(){
 
-console.log("hello")
+
 //wire up search button to pull city from ajax call for general search
 $(".search").on("click", 
-
 function searchCity(data){
+event.preventDefault()
 var searchbar = $(".searchInput").val()
 console.log(searchbar)
-
-
-
-
-// first ajax call with weather variables (temp humidity windspeed uvindex) for today's weather
-// function mainWeatherPull (searchbar){
-
-console.log(searchbar)
-
-$.ajax({
-url:"https://openweathermap.org/api" + searchbar + "889e002b8fmsh68a55d24a31d654p1e691ajsn4b5a65ae9eb2",
-method:"GET"
-}).then(function(dataPull){
-console.log(dataPull)
-})
+console.log("hello")
+weatherPull(searchbar)
+forecastPull(searchbar)
 })
 
+function weatherPull(searchbar) {
+    $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchbar + "&appid=80f453ffaaff3c7ad14e0dd13df083a0",
+    method: "GET"
+    }).then(function (response){
+    console.log(response)
+
+var city = $("<h2>").text(response.name)    
+var todayTemp = $("<h6>").addClass("todaystyle").text(response.main.temp)
+
+$(".todayWeather").append(city, todayTemp)
+})}
+
+
+// add next function here for 5 day weather same as before
+
+function forecastPull(searchbar) {
+    $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchbar + "&appid=80f453ffaaff3c7ad14e0dd13df083a0",
+    method: "GET"
+    }).then(function (response){
+    console.log(response)
 
 
 
 
-// second ajax call for five day weather forecast
-
-
-
-
-
-
-
+})}
 
 
 })
-
-
-
 
 
 
