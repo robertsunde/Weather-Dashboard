bde721a566d70f13a5b19f46d96ca3027aa06f79
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-01-16 15:11:02 -0500

    fixed formatting issues for todays weather

--- script.js
+++ script.js
@@ -2,38 +2,102 @@ $(document).ready(function(){
 
 
 //wire up search button to pull city from ajax call for general search
-$(".search").on("click", 
-function searchCity(data){
+$(".search").on("click",
+
+
+function searchCity(data){      
 event.preventDefault()
 var searchbar = $(".searchInput").val()
 console.log(searchbar)
-console.log("hello")
 weatherPull(searchbar)
-forecastPull(searchbar)
-})
+$(".todayWeather").empty();
 
+// forecastPull(searchbar)
+},
+)
+
+
+// function cleartodayWeather() {
+//     var remove = document.getElementById("remove")
+//     remove.classList.remove("todayWeather")  
+// },
+
+// function addtodayWeather() {
+//     var add = document.getElementById("remove")
+//     add.classList.add("todayWeather")  
+// }
+
+
+
+
+
+// first api pull for today's weather
 function weatherPull(searchbar) {
     $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchbar + "&appid=80f453ffaaff3c7ad14e0dd13df083a0",
+    url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchbar + "&appid=80f453ffaaff3c7ad14e0dd13df083a0&units=imperial",
     method: "GET"
     }).then(function (response){
     console.log(response)
 
+
+    // adds pulled data into html document under todayWeather class
 var city = $("<h2>").text(response.name)    
-var todayTemp = $("<h6>").addClass("todaystyle").text(response.main.temp)
+var todayTemp = $("<h1>").text(response.main.temp + " " + "F")
+var todayHumidity = $("<h6>").text("Humidity: " + response.main.humidity + "%")
+    
+// var todayTempConverted = "Temperature" + todayTemp + "F"
 
-$(".todayWeather").append(city, todayTemp)
-})}
+$(".todayWeather").prepend(city, todayTemp, todayHumidity)
 
+var cityName = $("<h2>").text(response.name)
+$(".lastSearch").prepend(cityName)
+
+    
 
 // add next function here for 5 day weather same as before
 
-function forecastPull(searchbar) {
-    $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchbar + "&appid=80f453ffaaff3c7ad14e0dd13df083a0",
-    method: "GET"
-    }).then(function (response){
-    console.log(response)
+// function forecastPull(searchbar) {
+//     $.ajax({
+//     url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchbar + "&appid=80f453ffaaff3c7ad14e0dd13df083a0",
+//     method: "GET"
+//     }).then(function (response){
+//     console.log(response)
+
+// })}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -41,7 +105,5 @@ function forecastPull(searchbar) {
 })}
 
 
+
 })
-
-
-
